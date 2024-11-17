9e958ddada2eaf11407de1d8d1eb48449477638e
Pankaj Patel <pantherpatel1980@gmail.com>
2024-11-17 20:29:53 +0530

    stripe issue fix.

--- src/app/site/page.tsx
+++ src/app/site/page.tsx
@@ -50,7 +50,7 @@ export default async function Home() {
           {" you're"} not <br />
           ready to commit you can get started for free.
         </p>
-        {/* <div className="flex  justify-center gap-4 flex-wrap mt-6">
+        <div className="flex  justify-center gap-4 flex-wrap mt-6">
           {prices.data.map((card) => (
             //WIP: Wire up free product from stripe
             <Card
@@ -153,7 +153,7 @@ export default async function Home() {
               </Link>
             </CardFooter>
           </Card>
-        </div> */}
+        </div>
       </section>
     </>
   )
